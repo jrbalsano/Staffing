e4ac7640f65e771704e21ed8985f3475718b5fe7
Jonathan Balsano <jrbalsano@gmail.com>
2012-03-29 11:25:03 -0400

    Added documentation and coverted format for Module.

--- Utils.js
+++ Utils.js
@@ -1,21 +1,48 @@
-function Utils() {
-  function forEach(arr, f) {
+var UTIL = (function Utils() {
+  var my = {};
+  
+  /**
+   * Runs a function f on each element of an array
+   * @param arr {Array} An array of items to cycle through
+   * @param f {Function} A function of the form function(value) which takes as
+   * parameter a value from the array.
+   */
+  my.forEach = function forEach(arr, f) {
     for(var i = 0; i < arr.length; i++)
       f(arr[i]);
   }
   
-  function reduce(combine, base, array) {
+  /**
+   * Reduces an array to a single value by use of a function, combine.
+   * @param combine {Function} A function of the form function(base, element)
+   * which uses the previous value of combine (base) and a new value from the
+   * array to return a new value of combine.
+   * @param base {Object} The initial value from which combine should work
+   * @param array {Array} The array which should be combined
+   * @return {Object} the value of base after combine has been run on each
+   * element in the array.
+   */
+  my.reduce = function reduce(combine, base, array) {
     forEach(array, function(element) {
       base = combine(base, element);
     });
     return base;
   }
   
-  function count(test, array) {
+  /**
+   * Counts the number of elements for which a certain condition is true in an
+   * array.
+   * @param test {Function} a function  of the form function(element) which
+   * returns true if the element meets the desired condition and false
+   * otherwise
+   * @param array {Array} The array in which to count elements that meet the
+   * condition
+   */
+  my.count = function count(test, array) {
     reduce(function(base, element) {
       return base + (test(element) ? 1 : 0);
     }, 0, array);
   }
   
-  
-}
+  return my;
+})();
