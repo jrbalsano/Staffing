e317a8132ca9dfaa645bc42776b6111bb9f27375
Jonathan Balsano <jrbalsano@gmail.com>
2012-03-29 22:43:49 -0400

    Made ConflictMatrix public Added Dictionary.remove
    Made the methods of ConflictMatrix public instead of protected and
    reformatted the generation of UTIL so that all methods are defined
    automatically and not via a function. Also added a method to the
    Dictionary for removing an existing value.

--- ConflictMatrix.js
+++ ConflictMatrix.js
@@ -1,22 +1,21 @@
 function ConflictMatrix(timeslots) {
-  var conflictLists = UTIL.fillArray(null, timeslots, function(){
+  this.conflictLists = UTIL.fillArray(null, timeslots, function(){
     return new UTIL.Dictionary();
   });
-  
-  this.setConflict = function(time1, time2) {
-    conflictLists[time1].store(time2, true);
-    conflictLists[time2].store(time1, true);
-  };
-  
-  this.getConflict = function(time1, time2) {
-    return conflictLists[time1].contains(time2);
-  };
-  
-  this.getConflicts = function(time) {
-    var ret = [];
-    UTIL.forEachIn(conflictLists[time], function(name, value) {
-      ret.push(name);
-    });
-    return ret;
-  };
 }
+ConflictMatrix.prototype.setConflict = function(time1, time2) {
+  this.conflictLists[time1].store(time2, true);
+  this.conflictLists[time2].store(time1, true);
+};
+
+ConflictMatrix.prototype.isConflict = function(time1, time2) {
+  return this.conflictLists[time1].contains(time2);
+};
+
+ConflictMatrix.prototype.getConflicts = function(time) {
+  var ret = [];
+  UTIL.forEachIn(this.conflictLists[time], function(name, value) {
+    ret.push(name);
+  });
+  return ret;
+};
--- Dictionary.js
+++ Dictionary.js
@@ -1,18 +1,24 @@
-var UTIL = (function(my) {
-  my.Dictionary = function Dictionary(startValues) {
-    this.values = startValues || {};
-    this.count = 0;
-  }
-  my.Dictionary.prototype.store = function(name, value) {
-    this.values[name] = value;
-    count++;
-  }
-  my.Dictionary.prototype.lookup = function(name) {
-    return this.values[name];
-  }
-  my.Dictionary.prototype.contains = function(name) {
-    return Object.prototype.hasOwnProperty.call(this, name);
-  }
-  
-  return my;
-})(UTIL || {});
+var UTIL = UTIL || {};
+UTIL.Dictionary = function Dictionary(startValues) {
+  this.values = startValues || {};
+  this.count = 0;
+};
+
+UTIL.Dictionary.prototype.store = function(name, value) {
+  this.values[name] = value;
+  count++;
+};
+
+
+UTIL.Dictionary.prototype.contains = function(name) {
+  return Object.prototype.hasOwnProperty.call(this, name);
+};
+
+UTIL.Dictionary.prototype.remove = function(key) {
+  if(this.contains(key)) delete this.values[key];
+  count--;
+}
+
+UTIL.Dictionary.prototype.lookup = function(name) {
+  return this.values[name];
+};
